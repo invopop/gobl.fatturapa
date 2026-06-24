2b98d14f12b3115f1328bf5c4d88246440f4d01a
Jose Rilla <jose@rilla.es>
2026-06-24 08:18:55 +0000

    Add it-sdi-notification extension

---
+++ addon/extensions_test.go
@@ -0,0 +1,23 @@
+package sdi_test
+
+import (
+	"testing"
+
+	sdi "github.com/invopop/gobl.fatturapa/addon"
+	"github.com/invopop/gobl/cbc"
+	"github.com/invopop/gobl/tax"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNotificationExtensionRegistered(t *testing.T) {
+	def := tax.ExtensionForKey(sdi.ExtKeyNotification)
+	require.NotNil(t, def, "it-sdi-notification must be registered")
+
+	got := make([]cbc.Code, 0, len(def.Values))
+	for _, v := range def.Values {
+		got = append(got, v.Code)
+	}
+	assert.ElementsMatch(t,
+		[]cbc.Code{"RC", "NS", "MC", "AT", "DT", "EC01", "EC02"}, got)
+}
